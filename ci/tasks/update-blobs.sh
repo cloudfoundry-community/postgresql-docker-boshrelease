3633618fb2074eceb9641cd5c4e96ad0d2876235
Dr Nic Williams <drnicwilliams@gmail.com>
2015-10-07 09:59:00 -0400

    upload blobs and commit to repo

--- ci/tasks/update-blobs.sh
+++ ci/tasks/update-blobs.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-echo ls image dir
-for image_dir in $(ls docker-image*); do
-  echo $image_dir
-  ls $image_dir/image
-done
+if [[ "${bosh_target}X" == "X" || "${aws_access_key_id}X" == "X" ]]; then
+  echo 'Require $bosh_target, $bosh_username, $bosh_password, $aws_access_key_id, $aws_secret_access_key'
+  exit 1
+fi
 
-echo ls image
+mkdir -p boshrelease/blobs/docker-images
+
 for image in $(ls docker-image*/image); do
   echo $image
+  cp $image boshrelease/blobs/docker-images/
 done
+
+cd boshrelease
+
+bosh -n upload blobs
+
+if [[ -z "$(git config --global user.name)" ]]
+then
+  git config --global user.name "Concourse Bot"
+  git config --global user.email "concourse-bot@starkandwayne.com"
+fi
+
+git commit -a -m "updated docker image blobs"
